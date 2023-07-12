0bd869e6e2e5d14d2516443605b9edbd63a97718
Jakub Olejnik <jaolejnik@protonmail.com>
2023-07-12 17:32:24 +0200

    Completed day05 2015

---
+++ src/2015/day05.cpp
@@ -0,0 +1,48 @@
+#include "../utils/io.h"
+
+#include <iostream>
+#include <regex>
+
+int part1(const std::vector<std::string> &input)
+{
+    int nice_count = 0;
+    std::regex vowels_re("([aeiou].*){3,}");
+    std::regex double_re("(.)\\1");
+    std::regex substr_re("ab|cd|pq|xy");
+
+    for (const std::string &s : input)
+    {
+        if (std::regex_search(s, vowels_re) &&
+            std::regex_search(s, double_re) &&
+            !std::regex_search(s, substr_re))
+            nice_count++;
+    }
+
+    return nice_count;
+}
+
+int part2(const std::vector<std::string> &input)
+{
+    int nice_count = 0;
+    std::regex double_double_re("(..).*\\1");
+    std::regex surround_re("(.).\\1");
+
+    for (const std::string &s : input)
+    {
+        if (std::regex_search(s, double_double_re) &&
+            std::regex_search(s, surround_re))
+            nice_count++;
+    }
+
+    return nice_count;
+}
+
+int main()
+{
+    auto input = read_input_vector_string(__FILE__);
+
+    std::cout << "Part 1: " << part1(input) << "\n";
+    std::cout << "Part 2: " << part2(input) << "\n";
+
+    return 0;
+}
--- src/utils/io.h
+++ src/utils/io.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <vector>
 
 namespace fs = std::filesystem;
 
@@ -18,14 +19,27 @@ std::string get_input_path(const std::string &current_file)
 
 std::string read_input_single_string(const std::string &current_file)
 {
-    std::string input_file = get_input_path(current_file);
-    std::ifstream myfile(input_file);
+    std::string input_path = get_input_path(current_file);
+    std::ifstream input_file(input_path);
     std::string input_string;
 
-    if (myfile.is_open())
-        std::getline(myfile, input_string);
+    if (input_file.is_open())
+        std::getline(input_file, input_string);
 
-    myfile.close();
+    input_file.close();
 
     return input_string;
+}
+
+std::vector<std::string> read_input_vector_string(const std::string &current_file)
+{
+    std::string input_path = get_input_path(current_file);
+    std::ifstream input_file(input_path);
+    std::string line;
+    std::vector<std::string> input_strings;
+
+    while (std::getline(input_file, line))
+        input_strings.push_back(line);
+
+    return input_strings;
 }
